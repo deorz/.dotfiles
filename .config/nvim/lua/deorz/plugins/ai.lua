27abeb7ddb242b8b7445f7eb52107cdda31ec092
Denis Orzhenovskiy <thedamnation1044@yandex.ru>
2024-08-31 15:51:43 +0300

    NEOVIM: Переписал получение ключа для ChatGPT.

--- .config/nvim/lua/deorz/plugins/ai.lua
+++ .config/nvim/lua/deorz/plugins/ai.lua
@@ -7,12 +7,15 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope.nvim",
 		},
-		opts = {
-			api_host_cmd = "echo -n api.proxyapi.ru/openai",
-			api_key_cmd = "echo -n sk-9RhpwIPNgPiMo7NJ37eiav8SVv6e3ovQ",
-			openai_params = {
-				model = "gpt-4o",
-			},
-		},
+		opts = function()
+			local home = vim.fn.expand("$HOME")
+			return {
+				api_host_cmd = "echo -n api.proxyapi.ru/openai",
+				api_key_cmd = "cat " .. home .. "/.config/nvim/key.txt",
+				openai_params = {
+					model = "gpt-4o",
+				},
+			}
+		end,
 	},
 }
