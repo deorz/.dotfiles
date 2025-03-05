0a74c3eaaea75182ea02c2ff5f061c75428fcd28
Denis Orzhenovskiy <thedamnation1044@yandex.ru>
2025-03-05 11:25:42 +0300

    NEOVIM: Добавил популярные темы.

--- nvim/dot-config/nvim/lua/deorz/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/deorz/plugins/colorscheme.lua
@@ -1,23 +1,23 @@
 return {
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	lazy = false,
-	-- 	name = "catppuccin",
-	-- 	priority = 1000,
-	-- 	opts = {
-	-- 		transparent_background = true,
-	-- 		integrations = {
-	-- 			mason = true,
-	-- 			snacks = true,
-	-- 			lsp_trouble = true,
-	-- 			notifier = true,
-	-- 		},
-	-- 	},
-	-- 	config = function(_, opts)
-	-- 		require("catppuccin").setup(opts)
-	-- 		vim.cmd.colorscheme("catppuccin")
-	-- 	end,
-	-- },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+			integrations = {
+				mason = true,
+				snacks = true,
+				lsp_trouble = true,
+				notifier = true,
+			},
+		},
+		-- config = function(_, opts)
+		-- 	require("catppuccin").setup(opts)
+		-- 	vim.cmd.colorscheme("catppuccin")
+		-- end,
+	},
 	{
 		"rose-pine/neovim",
 		lazy = false,
@@ -33,5 +33,18 @@ return {
 			require("rose-pine").setup(opts)
 			vim.cmd.colorscheme("rose-pine")
 		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		name = "tokyonight",
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+		-- config = function(_, opts)
+		-- 	require("tokyonight").setup(opts)
+		-- 	vim.cmd.colorscheme("tokyonight")
+		-- end,
 	},
 }
