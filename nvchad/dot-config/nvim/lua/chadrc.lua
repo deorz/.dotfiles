4d3192a000b316dfb986571e6d2a90624d4f8e54
Denis Orzhenovskiy <thedamnation1044@yandex.ru>
2025-05-18 13:19:05 +0300

    NEOVIM: Убрал tabbufline и добавил toggle для line wrap.

--- nvchad/dot-config/nvim/lua/chadrc.lua
+++ nvchad/dot-config/nvim/lua/chadrc.lua
@@ -9,6 +9,9 @@ M.base46 = {
 M.nvdash = { load_on_startup = true }
 
 M.ui = {
+  tabufline = {
+    enabled = false,
+  },
   statusline = {
     theme = "minimal",
     separator_style = "block",
--- nvchad/dot-config/nvim/lua/mappings.lua
+++ nvchad/dot-config/nvim/lua/mappings.lua
@@ -5,3 +5,5 @@ map("n", "<Esc>", "<cmd>noh<CR>", { desc = "Clear highlights" })
 
 map("v", "<", "<gv")
 map("v", ">", ">gv")
+
+map("n", "<leader>un", "<cmd>set wrap!<CR>", { desc = "Toggle line wrap" })
